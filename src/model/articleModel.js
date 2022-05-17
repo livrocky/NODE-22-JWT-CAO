3b905a8b4d7a823ae31017dd2ea70210d4af5d55
Mantas M <mantasef@gmail.com>
2022-05-17 10:23:53 +0300

    FE start

--- src/model/articleModel.js
+++ src/model/articleModel.js
@@ -15,11 +15,36 @@ async function getArrayFromDb(sql) {
   }
 }
 
+async function executeDb(sql, dataToDbArr) {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    const [result] = await conn.execute(sql, dataToDbArr);
+    return result;
+  } catch (error) {
+    console.log('error executeDb', error);
+    throw new Error('error executeDb');
+  } finally {
+    conn?.end();
+  }
+}
+
 async function getAllArticlesDb() {
   const sql = 'SELECT * FROM articles';
   return getArrayFromDb(sql);
 }
 
+async function insertArticleDb(newArticleObj) {
+  const { date, title, content } = newArticleObj;
+  // executeDb
+  const sql = `
+  INSERT INTO articles (date, title, content)
+  VALUES (?, ?, ?)
+  `;
+  return executeDb(sql, [date, title, content]);
+}
+
 module.exports = {
   getAllArticlesDb,
+  insertArticleDb,
 };
