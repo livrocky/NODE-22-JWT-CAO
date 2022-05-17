f08a766097887073e4e7bc0f4d2641cb2bd1cc22
rokas <rokasra@gmail.com>
2022-05-17 14:24:10 +0300

    fetch err

---
+++ public/js/articles.js
@@ -0,0 +1,31 @@
+const { getFetch } = require('./modules/fetch.js');
+
+/* eslint-disable no-param-reassign */
+const articlesListEl = document.getElementById('articles');
+
+// REGISTRUOTIEMS VARTOTOJAMS
+const token = localStorage.getItem('articleUserToken');
+console.log('token===', token);
+
+if (!token) {
+  // NEREGISTRUOTAS VARTOTOJAS , ATE
+  window.location.replace('login.html');
+}
+
+function renderArticles(arr, dest) {
+  dest.innerHTML = '';
+  arr.forEach((articleObj) => {
+    const liEl = document.createElement('li');
+    liEl.textContent = `${articleObj.title} - ${articleObj.content}`;
+    dest.append(liEl);
+  });
+}
+
+// GET and CONSOLE ALL ARTICLES
+
+async function getArticles(userToken) {
+  const articlesArr = await getFetch('allArticle', userToken);
+  console.log('articlesArr===', articlesArr);
+  renderArticles(articlesArr, articlesListEl);
+}
+getArticles(token);
---
+++ public/js/modules/fetch.js
@@ -0,0 +1,15 @@
+export const BASE_URL = 'http://localhost:3000';
+
+export async function getFetch(endpoint, token) {
+  try {
+    const resp = await fetch(`${BASE_URL}/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const dataInJs = await resp.json();
+    return dataInJs;
+  } catch (error) {
+    console.warn('error in getFetch', error);
+  }
+}
